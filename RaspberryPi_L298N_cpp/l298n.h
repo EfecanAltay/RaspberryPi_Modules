dac21e38b7085f22c502c839a4f8f75da07ac2aa
efecanaltay <efecan95.gs@gmail.com>
2017-03-29 10:04:36 +0000

    I've update l289n maincode

--- RaspberryPi_L298N_cpp/l298n.h
+++ RaspberryPi_L298N_cpp/l298n.h
@@ -9,14 +9,14 @@ using namespace std;
 
 class L298N{
 	public :
-		L298N(int,int,int,int);
-		L298N(int,int,int,int,int,int);
-		void setPwm1(int);
-		void setPwm2(int);
-		void forwardMotor1();
-		void backMotor1();
-		void forwardMotor2();
-		void backMotor2();
+		L298N(int,int,int,int);				//L298N(motor1_IN1,motor1_IN2,motor2_IN1,motor2_IN2)
+		L298N(int,int,int,int,int,int);		//L298N(motor1_IN1,motor1_IN2,motor2_IN1,motor2_IN2,motor1_pwm,motor2_pwm)
+		void setPwm1(int);					//if first you've setPwm(this is up row code),you can use this.
+		void setPwm2(int);					//this func. is same the up row func.
+		void forwardMotor1();				//you can turn forward motor1 
+		void backMotor1();					//you can turn back motor1
+		void forwardMotor2();				//you can turn forward motor2
+		void backMotor2();					//you can turn back motor2
 	private:
 		int m11,m12,m21,m22;
 		int pwm1,pwm2;
--- RaspberryPi_L298N_cpp/main.cpp
+++ RaspberryPi_L298N_cpp/main.cpp
@@ -3,8 +3,16 @@
 int main(){
 	L298N motors(25,24,23,22,29,28);
 	
-	motors.forwardMotor1();
-	motors.forwardMotor2();
+	motors.setPwm1(30);		// between to [0-100] 
+	motors.setPwm2(70);
+	
+	motors.forwardMotor1();	
+	motors.backMotor2();
+	
+	//.........
+	while(1){			
+		//............
+	}
 	
 	return 0 ;
 }
